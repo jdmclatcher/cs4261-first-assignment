222666552fa0e61e9cd1814d50e1c2960fb5fc1c
Jonathan McLatcher <jdmclatcher@gmail.com>
2024-01-12 21:43:24 -0500

    update env vars

--- FirstAssignment/config.js
+++ FirstAssignment/config.js
@@ -6,7 +6,16 @@ import {
 } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getApps, getApp, initializeApp } from "firebase/app";
-import firebaseConfig from "./firebaseConfig";
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
+};
 
 let firebaseApp;
 
