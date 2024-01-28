b5b4befa671e9f4be4c68c4cc8d75df05696c225
Jonathan McLatcher <jdmclatcher@gmail.com>
2024-01-28 16:20:37 -0500

    remove unnecessary files

--- FirstAssignment/config.js
+++ FirstAssignment/config.js
@@ -13,9 +13,7 @@ import {
   EXPO_PUBLIC_STORAGE_BUCKET,
   EXPO_PUBLIC_MESSAGING_SENDER_ID,
   EXPO_PUBLIC_APP_ID,
-  
 } from '@env';
-//EXPO_PUBLIC_MEASUREMENT_ID,
 const firebaseConfig = {
   apiKey: EXPO_PUBLIC_API_KEY,
   authDomain: EXPO_PUBLIC_AUTH_DOMAIN,
